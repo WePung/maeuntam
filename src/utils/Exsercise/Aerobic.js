f3b53b8bf29766b6f6a1d6458d0f41f57bf6d730
WePung <akgre5@naver.com>
2023-07-24 13:12:27 +0900

    Edit : ExerciseInfo
    정보 추가

--- src/utils/Exsercise/Aerobic.js
+++ src/utils/Exsercise/Aerobic.js
@@ -3,16 +3,34 @@ export const Aerobic = [
     value: "러닝",
     label: "러닝",
     level: 1,
+    info: "다이어트 효과, 체력 단련, 스트레스 해소, 불면증 해소, 정신 건강",
   },
   {
     value: "줄넘기",
     label: "줄넘기",
     level: 1,
+    info: "전신 운동, 근력 증대 및 운동 능력 향상, 골밀도 증가, 심장 건강 향상, 민첩성과 균형감각 향상",
+    src: "https://youtube.com/watch?v=8ki9cCy4Kko&feature=share",
+    step: [
+      {
+        id: 1,
+        how: "줄넘기 줄을 명치 높이로 맞춰줍니다.",
+      },
+      {
+        id: 2,
+        how: "줄을 돌리며 타이밍에 맞춰 앞꿈치로 점프를 해줍니다.",
+      },
+      {
+        id: 3,
+        how: "목표 갯수의 1/10 가량을 연속으로 하는 것을 목표로 설정하여 진행 합니다.",
+      },
+    ],
   },
   {
     value: "버피테스트",
     label: "버피테스트",
     level: 1,
+    info: "근력 향상,심폐 운동으로 작용,지방을 효과적으로 연소",
     src: "https://youtube.com/watch?v=K10SQGT-W1Q&feature=share",
     step: [
       {
@@ -33,6 +51,8 @@ export const Aerobic = [
     value: "브릿지",
     label: "브릿지",
     level: 3,
+    info: "허리통증 완화, 무릎 강화, 자세 교정, 힙업 효과, 균형 감각 향상, 뱃살 체지방 감소",
+    src: "https://youtu.be/BwHNiaee0D8",
     step: [
       {
         id: 1,
@@ -52,6 +72,8 @@ export const Aerobic = [
     value: "스위머",
     label: "스위머",
     level: 3,
+    info: "전신근육 강화,코어 힘 강화,척추 기립근 강화",
+    src: "https://youtu.be/_nNV7QtsSbo",
     step: [
       {
         id: 1,
